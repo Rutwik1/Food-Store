61de7c9f8e273bd3735b2014abe0e576a1450b16
Rutwik <rutkalbandhe@gmail.com>
2023-04-14 06:06:50 -0700

    first commit

---
+++ components/footer.js
@@ -0,0 +1,22 @@
+export const footer = () => {
+    return (
+        `<div id="footer">
+            <p class="text-center">Made with ❤️ by Rutwikk</p>
+            <div>
+                <button class="btn btn-primary">
+                    <a href="linkedin.com/in/rutwikkalbandhe" target="_blank">LinkedIn</a>
+                </button>
+                <button class="btn btn-success">
+                    <a href="https://github.com/Rutwik1" target="_blank">GitHub</a>
+                </button>
+                <button class="btn btn-danger">
+                    <a href="https://www.rutwikk.com" target="_blank">Portfolio</a>
+                </button>
+            </div>
+            <p class="text-center">Created By Rutwikk &copy; 2023 Food Store, <br />All Rights Reserved.</p><br />
+            
+            
+
+        </div>`
+    );
+};
